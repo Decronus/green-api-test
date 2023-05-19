8a97464d4daadc578b18c9a16d0c76f7b3b1b943
Decronus <zick3333@mail.ru>
2023-05-19 21:33:21 +0200

    queries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import MessageInput from "./components/message-input/MessageInput";
 import LoginModal from "./components/modals/login-modal/LoginModal";
 import AddChatModal from "./components/modals/add-chat-modal/AddChatModal";
 import Chat from "./components/chat/Chat";
+import axiosInstance from "./utils/axios";
+import { message } from "antd";
 
 function App() {
     const [loginModalVisibility, setLoginModalVisibility] = useState(false);
@@ -12,6 +14,7 @@ function App() {
     const [idInstance, setIdInstance] = useState("1101822802");
     const [apiTokenInstance, setApiTokenInstance] = useState("9a7fe7fd2eca4517aa6f3b9af3d08933494ff027e99940c1b8");
     const [currentContactPhone, setCurrentContactPhone] = useState(79991150041);
+    const [sendingMessage, setSendingMessage] = useState("");
     const [messages, setMessages] = useState({
         79991150041: [
             { text: "Text 1", me: false },
@@ -19,65 +22,111 @@ function App() {
             { text: "Text 3", me: true },
             { text: "Text 4", me: false },
             { text: "Text 1", me: false },
-            { text: "Text 2", me: true },
-            { text: "Text 3", me: true },
-            { text: "Text 4", me: false },
-            { text: "Text 1", me: false },
-            { text: "Text 2", me: true },
-            { text: "Text 3", me: true },
-            { text: "Text 4", me: false },
-            { text: "Text 1", me: false },
-            { text: "Text 2", me: true },
-            { text: "Text 3", me: true },
-            { text: "Text 4", me: false },
         ],
     });
+    console.log(messages);
 
-    const sendMessage = (message) => {
-        fetch(`https://api.green-api.com/waInstance${idInstance}/SendMessage/${apiTokenInstance}`, {
-            method: "POST",
-            body: JSON.stringify({
-                chatId: `${currentContactPhone}@c.us`,
-                message: "I use Green-API to send this message to you!",
-            }),
-        });
+    const scrollPageToBottom = () => {
+        window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+    };
 
+    const updateMessages = (message, me, contact = null) => {
         const newMessages = messages;
-        newMessages[currentContactPhone].push({ text: message, me: true });
+        newMessages[contact ?? currentContactPhone].push({ text: message, me: me });
         setMessages({ ...newMessages });
     };
 
+    const deleteNotification = (receiptId) => {
+        return axiosInstance.delete(
+            `https://api.green-api.com/waInstance${idInstance}/DeleteNotification/${apiTokenInstance}/${receiptId}`
+        );
+    };
+
+    const sendMessage = (message) => {
+        const body = {
+            chatId: `${currentContactPhone}@c.us`,
+            message: sendingMessage,
+        };
+        axiosInstance
+            .post(`https://api.green-api.com/waInstance${idInstance}/SendMessage/${apiTokenInstance}`, body)
+            .then(() => {
+                updateMessages(message, true);
+                setSendingMessage("");
+                scrollPageToBottom();
+            })
+            .catch(() => openMessage("error", "Неудачная отправка сообщения"));
+    };
+
+    const awaitMessage = () => {
+        axiosInstance
+            .get(`https://api.green-api.com/waInstance${idInstance}/ReceiveNotification/${apiTokenInstance}`)
+            .then((res) => {
+                console.log("data", res);
+                if (res.data) {
+                    const resBody = res.data.body;
+                    if (resBody.typeWebhook === "incomingMessageReceived") {
+                        const message = resBody.messageData.textMessageData.textMessage;
+                        const contact = resBody.senderData.chatId.split("@")[0];
+                        updateMessages(message, false, contact);
+                    }
+                    deleteNotification(res.data.receiptId);
+                }
+                // awaitMessage();
+                // setSendingMessage("");
+                // scrollPageToBottom();
+            })
+            .catch((error) => console.log("Ошибка при ожидании сообщения: ", error));
+    };
+
+    const [messageApi, messageApiContext] = message.useMessage();
+    const openMessage = (type, content) => {
+        messageApi.open({
+            type: type,
+            content: content,
+        });
+    };
+
+    useEffect(() => awaitMessage(), []);
+
     return (
-        <div className="App">
-            <Header
-                phone={currentContactPhone}
-                openLoginModal={() => setLoginModalVisibility(true)}
-                openAddChatModal={() => setAddChatModalVisibility(true)}
-            />
-            <Chat messages={messages["89991150041"]} />
+        <>
+            {messageApiContext}
 
-            <MessageInput sendMessage={sendMessage} />
-
-            {addChatModalVisibility && (
-                <AddChatModal
-                    visibility={addChatModalVisibility}
-                    currentContactPhone={currentContactPhone}
-                    setCurrentContactPhone={setCurrentContactPhone}
-                    closeModal={() => setAddChatModalVisibility(false)}
+            <div className="App">
+                <Header
+                    phone={currentContactPhone}
+                    openLoginModal={() => setLoginModalVisibility(true)}
+                    openAddChatModal={() => setAddChatModalVisibility(true)}
                 />
-            )}
+                <Chat messages={messages[`${currentContactPhone}`]} />
 
-            {loginModalVisibility && (
-                <LoginModal
-                    visibility={loginModalVisibility}
-                    idInstance={idInstance}
-                    apiTokenInstance={apiTokenInstance}
-                    setIdInstance={setIdInstance}
-                    setApiTokenInstance={setApiTokenInstance}
-                    closeModal={() => setLoginModalVisibility(false)}
+                <MessageInput
+                    sendMessage={sendMessage}
+                    sendingMessage={sendingMessage}
+                    setSendingMessage={setSendingMessage}
                 />
-            )}
-        </div>
+
+                {addChatModalVisibility && (
+                    <AddChatModal
+                        visibility={addChatModalVisibility}
+                        currentContactPhone={currentContactPhone}
+                        setCurrentContactPhone={setCurrentContactPhone}
+                        closeModal={() => setAddChatModalVisibility(false)}
+                    />
+                )}
+
+                {loginModalVisibility && (
+                    <LoginModal
+                        visibility={loginModalVisibility}
+                        idInstance={idInstance}
+                        apiTokenInstance={apiTokenInstance}
+                        setIdInstance={setIdInstance}
+                        setApiTokenInstance={setApiTokenInstance}
+                        closeModal={() => setLoginModalVisibility(false)}
+                    />
+                )}
+            </div>
+        </>
     );
 }
 
--- src/components/message-input/MessageInput.js
+++ src/components/message-input/MessageInput.js
@@ -1,25 +1,15 @@
 import { Input } from "antd";
 import "./style.css";
-import { useState } from "react";
 
-const MessageInput = ({ sendMessage }) => {
-    const [inputValue, setInputValue] = useState("");
-    const handleSendMessage = (event) => {
-        sendMessage(event);
-        setInputValue("");
-        console.log(document.body.scrollHeight);
-
-        setTimeout(window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" }), 0);
-    };
-
+const MessageInput = ({ sendMessage, sendingMessage, setSendingMessage }) => {
     return (
         <div className="message-input-block">
             <Input
                 style={{ border: 0, height: "40px" }}
-                value={inputValue}
-                onChange={(event) => setInputValue(event.target.value)}
+                value={sendingMessage}
+                onChange={(event) => setSendingMessage(event.target.value)}
                 placeholder="Введите сообщение"
-                onPressEnter={(event) => handleSendMessage(event.target.value)}
+                onPressEnter={(event) => sendMessage(event.target.value)}
             />
         </div>
     );
